7a58adbb67e404c7d4e7e925cd30e4246dae5595
Anton Hansson <hansson@google.com>
2019-01-18 14:13:10 +0000

    ginkgo: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: grep ro.sf.lcd_density vendor/build.prop
    Change-Id: I9f57a87a6c2d15c138dc3e6e7daa01bd5f5f950c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_OTA_ASSERT_DEVICE := ginkgo,willow
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
 
+# Display
+TARGET_SCREEN_DENSITY := 440
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_WILLOW_FILES := $(DEVICE_PATH)/configs/hidl/manifest_willow.xml
