9a5b994cc4aff675d42525fc70f4f01fae9c66fe
Introdructor <yashkachave7@gmail.com>
2020-12-14 13:50:24 +0000

    ginkgo:update vendorsetup for nfc repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,24 @@ rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf vendor/qcom/opensource/power
 rm -rf vendor/qcom/opensource/interfaces
+rm -rf hardware/broadcom/nfc
+rm -rf hardware/nxp/nfc
+rm -rf packages/apps/Nfc
+rm -rf vendor/nxp/opensource/interfaces/nfc
+rm -rf vendor/nxp/opensource/commonsys/external/libnfc-nci
+rm -rf vendor/nxp/opensource/commonsys/frameworks
+rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
+rm -rf vendor/nxp/opensource/sn100x/halimpl
+rm -rf vendor/nxp/opensource/sn100x/hidlimpl
+git clone https://github.com/LineageOS/android_hardware_broadcom_nfc.git hardware/broadcom/nfc
+git clone https://github.com/LineageOS/android_hardware_nxp_nfc.git hardware/nxp/nfc
+git clone https://github.com/LineageOS/android_packages_apps_Nfc.git packages/apps/Nfc
+git clone https://github.com/LineageOS/android_vendor_nxp_interfaces_opensource_nfc.git vendor/nxp/opensource/interfaces/nfc
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_external_libnfc-nci.git vendor/nxp/opensource/commonsys/external/libnfc-nci
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_frameworks.git vendor/nxp/opensource/commonsys/frameworks
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_packages_apps_Nfc.git vendor/nxp/opensource/commonsys/packages/apps/Nfc
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_halimpl.git -b lineage-18.0-sn100x vendor/nxp/opensource/sn100x/halimpl
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_hidlimpl.git -b lineage-18.0-sn100x vendor/nxp/opensource/sn100x/hidlimpl
 git clone https://github.com/ghostrider-reborn/android_hardware_qcom_display.git -b arrow-11.0-caf-sm6125-R hardware/qcom-caf/sm8150/display
 git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b  arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
