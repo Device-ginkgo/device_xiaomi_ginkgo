7a2a78ee3ef7d6cfa8d5a3b53171ecb8e875acdc
Introdructor <yashkachave7@gmail.com>
2021-04-10 02:02:33 +0000

    ginkgo:Aex Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_ginkgo.mk
+    $(LOCAL_DIR)/aosp_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_ginkgo-user \
-    revengeos_ginkgo-userdebug \
-    revengeos_ginkgo-eng
+    aosp_ginkgo-user \
+    aosp_ginkgo-userdebug \
+    aosp_ginkgo-eng
